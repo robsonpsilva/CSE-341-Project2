73401722f2a5c6fce8ff11aadc896445ff7d798a
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2025-03-27 19:40:27 -0300

    modified:   package-lock.json 	modified:   package.json 	modified:   server.js

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
+const passport = require("passport");
+const session = require("express-session");
+const giHubStrategy = require("password-github2").Strategy;
+const cors require("cors");
+
+
 const cors = require("cors");
 app.use(cors());
 
@@ -9,18 +15,57 @@ const mongodb = require("./data/database");
 
 const port = process.env.PORT || 8000;
 
-app.use(bodyParser.json());
-app.use((req,res,next) => {
+app
+.use(bodyParser.json())
+.use(session({
+    secret: "secret",
+    resave: false,
+    saveUninitialiazed: true,
+}))
+.use(passport.initialize())
+.use(passport.session())
+.use((req,res,next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Headers",
         "Origin, X-Request-With, Content-Type, Accept, Z-Key"
     );
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     next();
-});
-app.use("/", require("./routes"));
+})
+.use(cors({methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"]}))
+.use(cors({origin: "*"}))
+.use("/", "./routes/index.js")
 
+passport.use(new giHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: process.env.callback_url
 
+},
+function(accessToken, refreshToken, profile, done){
+    return done(null,profile)
+}
+));
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+})
+
+passport.deserializeUser(( user, done) => {
+    done(null, user);
+})
+
+// app.use("/", require("./routes"));
+
+app.get("/", (req, res) => { res.send(req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : "Logged Out")});
+
+app.get("/github/callback", passport.authenticate("github", { 
+    failureRedirect: "/api-docs", session: false}),
+    (req,res) => {
+        req.session.user = req.user;
+        res.redirect("/")
+    }
+);
 
 mongodb.initDb((err) =>{
     if(err){
